becf6bfde808c14dde14b23cea4961f39da1b48e
YeonJi <122419518+jiyeon0926@users.noreply.github.com>
2024-11-14 00:25:53 +0900

    Create schedule.sql

---
+++ schedule.sql
@@ -0,0 +1,23 @@
+-- user 테이블 생성
+CREATE TABLE `user` (
+  `userId` bigint NOT NULL COMMENT '유저 식별자',
+  `name` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '이름',
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '이메일',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '비밀번호',
+  `createdDate` timestamp NOT NULL COMMENT '등록일',
+  `modifiedDate` timestamp NOT NULL COMMENT '수정일',
+  PRIMARY KEY (`userId`)
+);
+
+-- schedule 테이블 생성
+CREATE TABLE `schedule` (
+  `id` bigint NOT NULL COMMENT '일정 식별자',
+  `userId` bigint NOT NULL COMMENT '유저 식별자',
+  `title` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '할일 제목',
+  `contents` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '할일 내용',
+  `createdDate` timestamp NOT NULL COMMENT '작성일',
+  `modifiedDate` timestamp NOT NULL COMMENT '수정일',
+  PRIMARY KEY (`id`),
+  KEY `schedule_user_FK` (`userId`),
+  CONSTRAINT `schedule_user_FK` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
+);
