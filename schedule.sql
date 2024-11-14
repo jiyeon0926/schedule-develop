e17a6022419ba6856cf4c67925aafc09abbd786b
YeonJi <122419518+jiyeon0926@users.noreply.github.com>
2024-11-14 15:10:14 +0900

    Update schedule.sql

--- schedule.sql
+++ schedule.sql
@@ -1,6 +1,6 @@
 -- user 테이블 생성
 CREATE TABLE `user` (
-  `userId` bigint NOT NULL COMMENT '유저 식별자',
+  `userId` bigint NOT NULL AUTO_INCREMENT COMMENT '유저 식별자',
   `name` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '이름',
   `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '이메일',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '비밀번호',
@@ -11,7 +11,7 @@ CREATE TABLE `user` (
 
 -- schedule 테이블 생성
 CREATE TABLE `schedule` (
-  `id` bigint NOT NULL COMMENT '일정 식별자',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '일정 식별자',
   `userId` bigint NOT NULL COMMENT '유저 식별자',
   `title` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '할일 제목',
   `contents` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '할일 내용',
